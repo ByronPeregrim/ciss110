97da120d599f02cb271b22b3de31d13f8200a31f
ByronPeregrim <byronperegrim@gmail.com>
2022-01-28 12:02:40 -0500

    first push of projects from intro to programming course. week 1 and 2

---
+++ week1/HelloByron.java
@@ -0,0 +1,9 @@
+public class HelloByron {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Byron");
+
+    }
+}
+
---
+++ week2/PeregrimAustralianOpen.java
@@ -0,0 +1,43 @@
+public class PeregrimAustralianOpen
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Byron Peregrim");
+        System.out.print("January 28 2022 11:55AM\n\n");
+        System.out.print("My favorite tennis players are:\n\tNovak Djokovic\n\tDanill Medvedev\n\tSimona Halep\n\n");
+
+        int wins = 20;
+        System.out.println("Roger Federer, Novak Djokovic and Rafael Nadal all currently have " + wins + " grand slam titles.\n");
+
+        double amount = 4.4;
+        System.out.println("The two winners of the Australian Open take home " + amount + " million dollars each.\n");
+
+        double attendance = 9.8512E4;
+        System.out.println("In 2021, only " + attendance + " people attended the Australian Open during the first ten days.\n");
+
+        boolean isEqual = true;
+        System.out.println("It is " + isEqual + " that both men and women earn equal pay at the Australian Open.\n");
+
+        char melbourne = 'M';
+        System.out.println(melbourne + " stands for Melbourne, the home of the Melbourne Sports and Entertainment Precinct.\n");
+
+        int rlCapacity = 14820, mcCapacity = 7500;
+        int arenaCapacity = rlCapacity + mcCapacity;
+        System.out.println("The total capacity of the Rod Laver and Margaret Court areana is " + arenaCapacity + ".\n");
+
+        int differences = rlCapacity - mcCapacity;
+        System.out.println("Rod Laver Arena can hold " + differences + " more people than Margaret Court Arena.\n");
+
+        double averagePriceRL = 125.0, dailyRLProfit = averagePriceRL * rlCapacity;
+        System.out.println("If Rod Laver Arena is filled to capacity and the average price of a day session\nticket is $" + averagePriceRL +
+         " they would make $" + dailyRLProfit + " a day.\n");
+
+        
+
+    
+
+
+
+
+    }
+}
